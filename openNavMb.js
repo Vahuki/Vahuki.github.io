ac12999c9bb4968869e0374d36b3a53cf54c4a63
Vahuki <kienhuu975@gmail.com>
2025-05-11 19:29:09 +0700

    block main when toggle navbar Mb

--- openNavMb.js
+++ openNavMb.js
@@ -1,10 +1,12 @@
 function openNavMb() {
     document.querySelector('nav').classList.toggle('open');
     document.querySelector('main').classList.toggle('opaciti-main');
+    document.body.classList.toggle('no-scroll'); // chặn scroll
 }
 document.querySelectorAll('header nav a').forEach(function(link) {
   link.addEventListener('click', function() {
     document.querySelector('nav').classList.remove('open');
     document.querySelector('main').classList.remove('opaciti-main');
+    document.body.classList.remove('no-scroll');
   });
 });
